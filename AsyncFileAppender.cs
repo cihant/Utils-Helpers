90916ee661cba53e33347b1708ab85f37cee4638
cihant <cihan@yucin.com>
2015-06-02 09:26:23 +0300

    Update AsyncFileAppender.cs

--- AsyncFileAppender.cs
+++ AsyncFileAppender.cs
@@ -1,4 +1,4 @@
-﻿using log4net;
+using log4net;
 using log4net.Appender;
 using log4net.Core;
 using System;
@@ -8,11 +8,32 @@ using System.Linq;
 using System.Text;
 
 
+/// <summary>
+///   <logger name="position">
+///     <level value="DEBUG" />
+///     <appender-ref ref="TESTLogger" />
+///   </logger>
+///   <appender name="TESTLogger" type="namespace.AsyncFileAppender">
+///     <lockingModel type="log4net.Appender.FileAppender+MinimalLock"/>
+///     <file value="Log\position.txt"/>
+///     <appendToFile value="true"/>
+///     <rollingStyle value="Date"/>
+///     <datePattern value="yyyyMMdd"/>
+///     <maxSizeRollBackups value="10"/>
+///     <layout type="log4net.Layout.PatternLayout">
+///       <conversionPattern value="%d{MM.dd.yyyy HH:mm:ss.fff} [%-5level] %logger // %message%newline"/>
+///     </layout>
+///     <filter type="log4net.Filter.LevelRangeFilter">
+///       <levelMin value="ALL"/>
+///       <acceptOnMatch value="true"/>
+///     </filter>
+///     <filter type="log4net.Filter.DenyAllFilter"/>
+///   </appender>
+/// </summary>
 public class AsyncFileAppender : RollingFileAppender
 {
 	volatile static bool running = false;
 	static ConcurrentBag<AsyncFileAppender> afAppenders = new ConcurrentBag<AsyncFileAppender>();
-	//static System.Collections.Queue logQueue = System.Collections.Queue.Synchronized(new System.Collections.Queue(1000, 1));
 	static System.Threading.AutoResetEvent mAutoResetEvent = new System.Threading.AutoResetEvent(true);
 
 	static int waitTime = 5 * 1000;
@@ -47,7 +68,7 @@ public class AsyncFileAppender : RollingFileAppender
 		if (running == false) { return; }
 		lock (this.lockObject)
 		{
-			this.loggingEvents.Add(loggingEvent);//.DoAppend(tuple.Item2);
+			this.loggingEvents.Add(loggingEvent);
 		}
 	}
 
